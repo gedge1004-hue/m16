d4245a0ddebd9921a35f1640d24753bdbdaa099f
SangHwa Kim <shkim@semi-ts.com>
2026-08-27 13:36:23 +0900

    백업

--- tests/02_monitoring/MON_0019_ControlState_Command_ONLINE_Remote.spec.ts
+++ tests/02_monitoring/MON_0019_ControlState_Command_ONLINE_Remote.spec.ts
@@ -20,15 +20,15 @@ test('MON_0019 Control state - ON-LINE Remote', async ({ page }) => {
     // Monitoring 페이지 진입
     await menuPage.navigateTo('monitoring');
     // Control online remote 클릭
-    await equipment.conState('onRemote', 'y');
+    await equipment.conState('onRemote', 'ok');
     // Control offline 클릭
-    await equipment.conState('offline', 'y');
+    await equipment.conState('offline', 'ok');
     // Control state 값 영역
     const stateValueBox = page.locator('.state-row').nth(1).locator('.state-value');
     // Control offline 및 배경까지 이미지 비교
     await expect(stateValueBox).toHaveScreenshot('control_offline_remote.png');
     // Control online remote 클릭
-    await equipment.conState('onRemote', 'y');
+    await equipment.conState('onRemote', 'ok');
     // Control online remote 및 배경까지 이미지 비교
     await expect(stateValueBox).toHaveScreenshot('control_online_remote.png');
 
